6c4e41911ded3cb85b0767e5931b115c4063d942
MarcoADeCSilva <marcoacsilva@live.com>
2021-09-16 22:14:59 -0300

    Adicionado entidades, ValueObjects

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,10 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using Paymentcontext.Shared.ValueObjects;
+using PaymentContext.Domain.ValueObjects;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Student
     {
-        public string FirstNome { get; private set; }
-        public string LastName { get; private set; }
-        public string Document { get; private set; }
-        public string Email { get; private set; }
+        private IList<Subscription> _subscriptions;
+
+        public Student(Name name, Document document, Email email)
+        {
+            Name = name;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public Name Name { get; private set; }        
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
+        public Address Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get {return _subscriptions.ToArray();}}
+
+        public void AddSubscription(Subscription subscription)        
+        {
+            foreach(var sub in Subscriptions)
+                sub.Inactivate();
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
